b8e5de3285f06b8a2f15c0b4c0c125a23e9b8407
Lee-Ann Seegets <69766648+lseegets@users.noreply.github.com>
2023-06-08 16:49:39 +0200

    add more weights + textmesh updates

---
+++ Assets/Scripts/LaserInput.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserInput : MonoBehaviour
+{
+
+    public static GameObject currentObject;
+    int currentID;
+
+    private float range = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = null;
+        currentID = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, range);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            int id = hit.collider.gameObject.GetInstanceID();
+
+            if (currentID != id)
+            {
+                currentID = id;
+                currentObject = hit.collider.gameObject;
+
+                string name = currentObject.name;
+                if (name == "Next")
+                {
+                    Debug.Log("HIT NEXT");
+                }
+
+                if (currentObject.CompareTag("Button"))
+                {
+                    Debug.Log("HIT BUTTON");
+                }
+            }
+        }
+    }
+}
