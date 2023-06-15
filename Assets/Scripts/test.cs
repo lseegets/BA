ab4221c99bd9c94068cd1d4aff97537b157ddeda
Lee-Ann Seegets <69766648+lseegets@users.noreply.github.com>
2023-06-15 16:49:42 +0200

    add DumbbellContainer prefab

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Rigidbody rb;
+    public Transform controller;
+    [Range(0.0f, 360.0f)]
+    public float rotateBy;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.MovePosition(controller.position);
+        rb.MoveRotation(controller.rotation * Quaternion.Euler(rotateBy, 0, 0));
+    }
+}
