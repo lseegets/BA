6f6124b484a188b06d7f2a05430943e9d58bfef8
Lee-Ann Seegets <69766648+lseegets@users.noreply.github.com>
2023-06-20 21:07:22 +0200

    add tracker functionality

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Controller : MonoBehaviour
 {
     Rigidbody rb;
     public Transform controller;
