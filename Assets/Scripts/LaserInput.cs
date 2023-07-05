0608e3955d0fe2e8d801367955e9e3266c221041
Lee-Ann Seegets <69766648+lseegets@users.noreply.github.com>
2023-07-05 18:12:54 +0200

    add more models

--- Assets/Scripts/LaserInput.cs
+++ Assets/Scripts/LaserInput.cs
@@ -6,19 +6,16 @@ public class LaserInput : MonoBehaviour
 {
 
     public static GameObject currentObject;
-    int currentID;
 
     public Material activatedMaterial;
     public Material defaultMaterial;
 
-    private float range = 100.0f;
     private float countdown = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         currentObject = null;
-        currentID = 0;
     }
 
     // Update is called once per frame
@@ -34,50 +31,18 @@ public class LaserInput : MonoBehaviour
             if (countdown < 0)
             {
                 Destroy(currentObject);
-                resetCountdown();
+                ResetCountdown();
             }
         }
         else
         {
-            resetCountdown();
+            ResetCountdown();
             if (currentObject != null) currentObject.GetComponent<MeshRenderer>().material = defaultMaterial;
         }
       }
-      
-
-
-       /* RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position, transform.forward, range);
-
-        for (int i = 0; i < hits.Length; i++)
-        {
-            RaycastHit hit = hits[i];
-            int id = hit.collider.gameObject.GetInstanceID();
-
-            if (currentID != id)
-            {
-                currentID = id;
-                currentObject = hit.collider.gameObject;
-
-                if (currentObject.CompareTag("Target"))
-                {
-                    countdown -= Time.deltaTime;
-                    if (countdown < 0)
-                    {
-                        Destroy(currentObject);
-                        resetCountdown();
-                    }
-                }
-                else if (!currentObject.CompareTag("Target"))
-                {
-                    //resetCountdown();
-                    Debug.Log("hitting canvas");
-                }
-            }
-        }*/
     }
 
-    private void resetCountdown()
+    private void ResetCountdown()
     {
         countdown = 2.0f;
     }
